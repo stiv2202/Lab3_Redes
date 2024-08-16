278196e8b4a37962601d0e3292722b3b10ecfb40
TheKiesling <kie21581@uvg.edu.gt>
2024-08-16 17:23:32 -0600

    feat: link-state

--- consts.js
+++ consts.js
@@ -1,7 +1,8 @@
 const domainName = "alumchat.lol"
 
 // Definir el algoritmo a utilizar en la ejecución del programa.
-const ALGORITHM = 'distance-vector'; 
+//const ALGORITHM = 'distance-vector'; 
 //const ALGORITHM = 'dijkstra';
+const ALGORITHM = 'link-state';
 
 module.exports = { domainName, ALGORITHM };
